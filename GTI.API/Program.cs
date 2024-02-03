74ad0d550a0c7b34bef184b177dfbf9e90d07bcb
Christian Souza <chrsouza012@gmail.com>
2024-02-03 16:35:32 -0300

    Commands, Handles, gitignore

--- GTI.API/Program.cs
+++ GTI.API/Program.cs
@@ -1,7 +1,12 @@
+using GTI.Domain.Entities;
 using GTI.Infra;
+using GTI.Infra.Data.Interfaces;
+using GTI.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<GTIDataContext>(options =>
@@ -26,3 +31,31 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<GTIDataContext>(options =>
+    {
+        options.UseSqlServer(connectionString, x => x.MigrationsAssembly("GTI.Infra"));
+    });
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var services = GetServiceCollection(builder);
+}
+
+IServiceCollection GetServiceCollection(WebApplicationBuilder builder)
+{
+    // Adicionando serviços
+    var services = builder.Services;
+    services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+    services.AddScoped<IReadRepository<Cliente>, ApplicationRepository<Cliente>>();
+    services.AddScoped<IWriteRepository<Cliente>, ApplicationRepository<Cliente>>();
+    services.AddScoped<IReadRepository<Endereco>, ApplicationRepository<Endereco>>();
+    services.AddScoped<IWriteRepository<Endereco>, ApplicationRepository<Endereco>>();
+
+    return services;
+}
